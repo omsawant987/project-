a8243e10170358865e47b4b0bcef85c96f442468
omsawant987 <69904001+omsawant987@users.noreply.github.com>
2020-11-28 20:02:37 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,17 @@
+class Plinkos{
+    constructor(x ,y){
+        var options = {
+            isStatic: true
+        }
+        this.radius = 10;
+        this.body = Bodies.circle(x,y,this.radius,options);
+        
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        ellipseMode(RADIUS)
+        fill("white");
+        ellipse(pos.x,pos.y,this.radius,this.radius);
+    }
+}
